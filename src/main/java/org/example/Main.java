38b8c12e11c0d74b651864f5669c7617f76c9b8a
David Hormigo Ramírez <dhorram948@g.educaand.es>
2022-11-02 19:22:51 +0100

    Primer Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        int contador = 100;
+        while (contador >0 ) {
+            contador--;
+
+            System.out.println(contador);
+        }
+    }
+}
